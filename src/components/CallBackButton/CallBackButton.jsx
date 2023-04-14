e290bb90b1d9a143fd2e33a1df4e6b5c726a20bf
Khrystyna Naumova <khrystyna.naumova@gmail.com>
2023-04-14 18:30:21 +0300

    modals_callback

--- src/components/CallBackButton/CallBackButton.jsx
+++ src/components/CallBackButton/CallBackButton.jsx
@@ -1,43 +1,46 @@
 import React, { useState } from "react";
 import { ReactComponent as Phone } from "../../assets/svg/phone-call.svg";
 import s from "./CallbackButton.module.scss";
+import { ReactComponent as Send } from "../../assets/svg/send.svg";
 
 const CallBackButton = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [phoneNumber, setPhoneNumber] = useState('+38 (0__) ___ __ __');
+  const [phoneNumber, setPhoneNumber] = useState("+38 (0__) ___ __ __");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsOpen(false);
     console.log(phoneNumber);
-    setPhoneNumber('+38 (0__) ___ __ __');
+    setPhoneNumber("+38 (0__) ___ __ __");
   };
 
   const changePhoneHandler = (e) => {
-    const cleanedValue = e.target.value.replace(/\D/g, '');
+    const cleanedValue = e.target.value.replace(/\D/g, "");
     // let formattedValue = '+38 (0__) ___ __ __';
     // for (let i = 0; i < cleanedValue.length && i < 8; i++) {
     //   formattedValue = formattedValue.replace('_', cleanedValue[i]);
     // }
 
     setPhoneNumber(cleanedValue);
-  
   };
 
   return (
     <>
-      <div className={s.callButton} onClick={() => setIsOpen(prevIsOpen => !prevIsOpen)}>
+      <div
+        className={s.callButton}
+        onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
+      >
         <Phone />
       </div>
       {isOpen && (
         <div className={s.callBackModal}>
           <div className={s.callBackModalContent}>
-            <p style={{marginLeft:'10px'}}>Продукція:</p>
+            <p style={{ marginLeft: "10px" }}>Продукція:</p>
             <p>+38 (050) 395 31 45</p>
-            <p style={{marginLeft:'10px'}}>Сайт</p>
+            <p style={{ marginLeft: "10px" }}>Сайт</p>
             <p>+38 (095) 101 50 79</p>
             <br></br>
-            <p style={{marginLeft:'10px'}}>Зателефонувати мені:</p>
+            <p style={{ marginLeft: "10px" }}>Зателефонувати мені:</p>
             <form onSubmit={handleSubmit}>
               <input
                 type="tel"
@@ -47,7 +50,10 @@ const CallBackButton = () => {
                 onChange={changePhoneHandler}
                 value={phoneNumber}
               />
-              <button  className={s.callBackModal_btn}type="submit">О</button>
+
+              <button className={s.callBackModal_btn} type="submit">
+                <Send />
+              </button>
             </form>
           </div>
         </div>
