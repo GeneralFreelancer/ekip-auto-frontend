1afeb8c4e61943222eeabb73669624b15897d5a2
FreelancerDen <freelancer.den.rud@gmail.com>
2023-04-27 15:15:33 +0300

    add some changes: logo size, navbar menu items, footer

--- src/components/Navbar/NavbarLinkLogo/NavbarLink/NavbarLink.jsx
+++ src/components/Navbar/NavbarLinkLogo/NavbarLink/NavbarLink.jsx
@@ -7,14 +7,20 @@ const NavbarLink = ({styleItem}) => {
   return (
     <div className={styleItem ? `${s[`${styleItem}`]} ${s.logo_navlink}` : s.logo_navlink}>
       <div className={s.nav_links}>
-        <div className={s.nav_li}>
-          <Link to="/about">Про нас</Link>
+        <div className={s.nav_links_wrapper}>
+          <div className={s.nav_li}>
+            <Link to="/about">Про нас</Link>
+          </div>
         </div>
-        <div className={s.nav_li}>
-          <Link to="/partners">Для партнерів</Link>
+        <div className={s.nav_links_wrapper}>
+          <div className={s.nav_li}>
+            <Link to="/partners">Для партнерів</Link>
+          </div>
         </div>
-        <div className={s.nav_li}>
-          <Link to="/delivery">Доставка</Link>
+        <div className={s.nav_links_wrapper}>
+          <div className={s.nav_li}>
+            <Link to="/delivery">Доставка</Link>
+          </div>
         </div>
       </div>
     </div>
