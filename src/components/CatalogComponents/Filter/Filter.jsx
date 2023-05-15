6f5a0cac34c5e09bbacca8e3edefdf7be1bda36b
TanyaDuka <duka.tanik@gmail.com>
2023-05-15 17:53:41 +0300

    fix

--- src/components/CatalogComponents/Filter/Filter.jsx
+++ src/components/CatalogComponents/Filter/Filter.jsx
@@ -4,24 +4,25 @@ const Filter = ({ onChangeParams }) => {
   return (
     <div className={s.wrapper}>
       <p className={s.title}>Спочатку:</p>
-      <select
-        name="filter"
-        className={s.select}
+      <div
+        className={s.wrapperSelect}
         onChange={(e) => onChangeParams(e.target.name, e.target.value)}
       >
-        <option value="new" className={s.option}>
-          Нове
-        </option>
-        <option value="popular" className={s.option}>
-          За популярністю
-        </option>
-        <option value="cheap" className={s.option}>
-          Дешеві
-        </option>
-        <option value="expensive" className={s.option}>
-          Дорожчі
-        </option>
-      </select>
+        <select name="filter" className={s.select}>
+          <option value="new" className={s.option}>
+            Нове
+          </option>
+          <option value="popular" className={s.option}>
+            За популярністю
+          </option>
+          <option value="cheap" className={s.option}>
+            Дешеві
+          </option>
+          <option value="expensive" className={s.option}>
+            Дорожчі
+          </option>
+        </select>
+      </div>
     </div>
   );
 };
